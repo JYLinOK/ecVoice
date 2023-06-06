48b16ce743c5be9ebd39b58d62eb624da684370a
YDOOK <35529779+JYLinOK@users.noreply.github.com>
2023-06-06 17:56:30 +0800

    Update Notes

--- useWhisper.py
+++ useWhisper.py
@@ -3,15 +3,15 @@ import whisper
 
 def getHanWord(audio_F, model='l'):
     if model == 't':
-        Model = whisper.load_model("tiny")  # tiny ~= 1GB -> 1-2GB
+        Model = whisper.load_model("tiny")  # VRAM  ~= 1GB -> 1-2GB
     elif model == 'b':
-        Model = whisper.load_model("base")  # tiny ~= 1GB -> 2-4GB
+        Model = whisper.load_model("base")  # VRAM  ~= 1GB -> 2-4GB
     elif model == 's':
-        Model = whisper.load_model("small")  # tiny ~= 2GB -> 4-6GB
+        Model = whisper.load_model("small")  # VRAM  ~= 2GB -> 4-6GB
     elif model == 'm':
-        Model = whisper.load_model("medium")  # tiny ~= 5GB -> 6-8GB
+        Model = whisper.load_model("medium")  # VRAM  ~= 5GB -> 6-8GB
     elif model == 'l':
-        Model = whisper.load_model("large")  # tiny ~= 5GB -> >12GB
+        Model = whisper.load_model("large")  # VRAM  ~= 10GB -> >12GB
 
     Hanzi = Model.transcribe(audio_F, language='zh', verbose=False, initial_prompt="简体")
     return ", ".join([i["text"] for i in Hanzi["segments"] if i is not None])
